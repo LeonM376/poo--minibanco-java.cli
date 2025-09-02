d6f06f664e4b3b2ef1af52092351169e9e694c2c
LeonM376 <cuervusssj@gmail.com>
2025-09-02 18:12:39 +0000

    Actividad Poo:Minbanco inicial

--- src/org/ejercicio/banco/Main.java
+++ src/org/ejercicio/banco/Main.java
@@ -2,8 +2,12 @@ package org.ejercicio.banco;
 
 public class Main{
     public static void main(String[] args) {
-        Cliente clientel = new Cliente("Luis Gomez", 25672946);
-        CuentaBancaria cuental = new CuentaBancaria(693531,clientel,10000);
+        Cliente cliente1 = new Cliente("Luis Gomez", 25672946);
+        CuentaBancaria cuenta1 = new CuentaBancaria(693531,clientel,10000);
+
+        Cliente cliente2 = new Cliente("Jose Luis",333);
+
+        CuentaBancaria cuenta2 = new CuentaBancaria(123,new Cliente("Juan Perez",234),1200)
 
     
     }
